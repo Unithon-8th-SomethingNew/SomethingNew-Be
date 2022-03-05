c817ce55f988c5374215fd70459c35f33275fc8b
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-03-05 15:56:36 +0900

    feat: userId로 친구주소 찾기 구현 #8

--- src/main/java/unithon8th/somethingnew/api/UserController.java
+++ src/main/java/unithon8th/somethingnew/api/UserController.java
@@ -7,16 +7,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import unithon8th.somethingnew.domain.user.User;
 import unithon8th.somethingnew.domain.user.UserRepository;
+import unithon8th.somethingnew.dto.friend.FriendLocationResponseDto;
 import unithon8th.somethingnew.dto.user.UserCallableRequestDto;
 import unithon8th.somethingnew.dto.user.UserLocationResponseDto;
+import unithon8th.somethingnew.service.FriendService;
 import unithon8th.somethingnew.service.UserService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
 public class UserController {
     private final UserService userService;
-    private final UserRepository userRepository;
+    private final FriendService friendService;
 
     //cancall 변경 api
     @PutMapping("/callable")
@@ -25,15 +29,4 @@ public class UserController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @GetMapping("/location/{userId}")
-    public ResponseEntity<UserLocationResponseDto> returnUserLocation(@PathVariable("userId") Long userId) {
-        try {
-            UserLocationResponseDto userLocationDto = userService.findUserLocation(userId);
-            Assert.notNull(userLocationDto);
-
-            return ResponseEntity.ok(userLocationDto);
-        }catch (Exception e){
-        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-    }
-}
 }
---
+++ src/main/java/unithon8th/somethingnew/dto/friend/UserFriendResponseDto.java
@@ -0,0 +1,24 @@
+package unithon8th.somethingnew.dto.friend;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserFriendResponseDto {
+    private Long userId;
+    private String username;
+    private String email;
+    private String imgUrl;
+    private boolean canCall;
+    private String street;
+
+    @Builder
+    public UserFriendResponseDto(Long userId, String username, String email, String imgUrl, boolean canCall, String street) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.imgUrl = imgUrl;
+        this.canCall = canCall;
+        this.street = street;
+    }
+}
