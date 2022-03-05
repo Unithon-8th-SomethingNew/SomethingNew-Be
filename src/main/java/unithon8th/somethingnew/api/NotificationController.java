0e6c1c6b54a724ec6f256635c9634fdbdc5a06c7
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-03-05 18:52:33 +0900

    feat: canCall이 false 일때 knock 방지 #5

--- src/main/java/unithon8th/somethingnew/api/NotificationController.java
+++ src/main/java/unithon8th/somethingnew/api/NotificationController.java
@@ -9,11 +9,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import unithon8th.somethingnew.domain.user.User;
 import unithon8th.somethingnew.dto.nitification.NotificationRequestDto;
 import unithon8th.somethingnew.service.fcm.AndroidPushNotificationsService;
 import unithon8th.somethingnew.service.fcm.AndroidPushPeriodicNotifications;
 import unithon8th.somethingnew.service.UserService;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -26,28 +28,30 @@ public class NotificationController {
     @Autowired
     AndroidPushNotificationsService androidPushNotificationsService;
 
-    @GetMapping(value = "/knock")
+    @PostMapping(value = "/knock")
     public @ResponseBody ResponseEntity<String> send(@RequestBody NotificationRequestDto notificationRequestDto) throws JSONException, InterruptedException  {
-        AndroidPushPeriodicNotifications androidPushPeriodicNotifications = new AndroidPushPeriodicNotifications(userService);
-        String notifications = androidPushPeriodicNotifications.PeriodicNotificationJson(notificationRequestDto);
+        Optional<User> optionalUser = userService.findUserByUserId(notificationRequestDto.getToUserId());
+        if(optionalUser.isPresent() && optionalUser.get().isCanCall() == true) {
+            AndroidPushPeriodicNotifications androidPushPeriodicNotifications = new AndroidPushPeriodicNotifications(userService);
+            String notifications = androidPushPeriodicNotifications.PeriodicNotificationJson(notificationRequestDto);
 
-        HttpEntity<String> request = new HttpEntity<>(notifications);
+            HttpEntity<String> request = new HttpEntity<>(notifications);
 
-        CompletableFuture<String> pushNotification = androidPushNotificationsService.send(request);
-        CompletableFuture.allOf(pushNotification).join();
+            CompletableFuture<String> pushNotification = androidPushNotificationsService.send(request);
+            CompletableFuture.allOf(pushNotification).join();
 
-        try{
-            String firebaseResponse = pushNotification.get();
-            return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
-        }
-        catch (InterruptedException e){
-            logger.debug("got interrupted!");
-            throw new InterruptedException();
-        }
-        catch (ExecutionException e){
-            logger.debug("execution error!");
-        }
 
-        return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
+            try {
+                String firebaseResponse = pushNotification.get();
+                return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
+            } catch (InterruptedException e) {
+                logger.debug("got interrupted!");
+                throw new InterruptedException();
+            } catch (ExecutionException e) {
+                logger.debug("execution error!");
+            }
+
+            return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
+        } else return new ResponseEntity<>("toUser's isCanCall is FALSE!",HttpStatus.NOT_FOUND);
     }
 }
